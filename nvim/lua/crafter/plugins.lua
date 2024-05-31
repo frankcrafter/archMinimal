340d73016a14ebb78161b6d8ec7d9efe22ea150c
Frank Marchese <frankcoding97@gmail.com>
2024-05-31 03:30:22 -0700

    Add files via upload

--- nvim/lua/crafter/plugins.lua
+++ nvim/lua/crafter/plugins.lua
@@ -74,8 +74,6 @@ return require("packer").startup(function(use)
 	use("hrsh7th/cmp-cmdline")
 	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
 
-
-
 	--- AutoPairs
 	use({
 		"windwp/nvim-autopairs",
